a4633fad9dc51e165932004b89de8a2566e6e172
Yutian Chen <markchenyutian@gmail.com>
2026-01-24 11:55:45 -0500

    Use MapAnything-v1 checkpoint for compatability.

--- Network/map_anything/__init__.py
+++ Network/map_anything/__init__.py
@@ -4,4 +4,4 @@ from .mapanything.models import MapAnything
 def get_MapAnything():
     if (pretrained_path := Path("./.torch_cache/mapanything.pth")).exists():
         return MapAnything.from_pretrained(pretrained_path)
-    return MapAnything.from_pretrained("facebook/map-anything")
+    return MapAnything.from_pretrained("facebook/map-anything-v1")
